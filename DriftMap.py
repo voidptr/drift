9264e005b39bb2c67a1ebf316c04004ad8aeb112
voidptr <git@voidptr.net>
2012-06-20 22:45:20 -0400

    in process adding map functionality

--- DriftMap.py
+++ DriftMap.py
@@ -1,35 +1,46 @@
+class FeatureTypes:
+    """
+    The types of features one may encounter on the map
+    """
+    def __init__(self, id, description, activities)
+        self.id = id
+        self.description = description
+        self.activities = activities  # keywords that will trigger certain behaviors
+
+    @staticmethod
+    def feature_type_builder(file):
+        features = []
+        fh = open(file)
+        for line in fh:
+            line = line.strip()
+            if len(line) == 0 or line[0] == "#":
+                continue
+
+            line = line.split()
+            features.append(FeatureTypes(line[0], line[1], line[2:]))
+        fh.close()
+        return features
+
+class LandTypes:
+    """
+    The types of landscapes available
+    """
+
 class DriftMap:
     """
     Holds and manages the world map
     (model) - static, for the most part... that is unless you've dug a hole or something.
-    """    
+    """
     class Level:
         """
         Individual Levels - stuff that is separated by entrances
         """
         class Area:
             """
-            Area Polygons
+            Area Squares
             """
-# TODO Iteration 2
-#            class Door:
-#                """
-#                A generic conduit between levels.
-#                """
-#                def __init__(self, name, coordinates, direction_of_travel=None, outgoing_level, outgoing_coordinates, description_text)
-#                    self.name = name
-#                    self.coordinates = coordinates
-#                    self.direction_of_travel = direction_of_travel
-#                    self.outgoing_level = outgoing_level
-#                    self.outgoing_coordinates = outgoing_coordinates
-
             def __init__(self, name, border_coordinates, traversal_timestep, description_text, view_distance):
-                self.name = name
                 self.border_coordinates = border_coordinates
-                self.traversal_timestep = traversal_timestep
-                self.description_text = description_text
-                self.view_distance = view_distance
-#                self.doors = []  # TODO Iteration 2
 
         def __init__(self, name, datafile):
             self.name = name
@@ -47,17 +58,6 @@ class DriftMap:
                 line = line.split(",")  # csv
 
                 name = line[0]
-#                type = line[1]
-# TODO Iteration 2
-#                if type == "Door"
-#                    coordinates = line[2]
-#                    direction_of_travel = line[3]
-#                    outgoing_level = line[4]
-#                    outgoing_coordinates = line[5]
-#                    description_text = line[6]
-#
-#                    self.areas[-1].doors.append( self.Door( name, coordinates, direction_of_travel, outgoing_level, outgoing_coordinates, description_text ) )
-#                else:
                 traversal_timestep = line[2]
                 description_text = line[3]
                 border_coordinates = line[4:]
@@ -72,5 +72,5 @@ class DriftMap:
         self.levels = {}
         for (name, file) in level_files:
             self.levels[name] = DriftMap.Level(name, file)
-            
+
 
